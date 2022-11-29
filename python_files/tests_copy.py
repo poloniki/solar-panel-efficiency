aec002cb89874b579760f404ad6ccf1ed7c65e60
aleix camps <aleixcf99@gmail.com>
2022-11-29 19:08:09 +0100

    Separated tests.py for computational power

--- python_files/tests_copy.py
+++ python_files/tests_copy.py
@@ -7,7 +7,6 @@ import numpy as np
 import pandas as pd
 
 if __name__ == "__main__":
-    # SUPERVISED MODEL WITH 1+ YEAR
-    df_sup = get_dataframe_option3()
-    (df_to_model(df_sup,0,supervised=True)).to_csv("allyears_supervised_df.csv")
-    print("--------------COMPLETED SUPERVISED MODEL ----------------")
+    # UNSUPERVISED MODEL WITH 5 YEARS
+    df_unsup = get_dataframe_option1()
+    (df_to_model(df_unsup,2015, supervised=False)).to_csv("10y_unsupervised_df.csv")
