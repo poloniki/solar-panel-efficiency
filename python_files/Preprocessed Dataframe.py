63a09239cddc0d720b42dff4528c97e713d24ac5
aleix camps <aleixcf99@gmail.com>
2022-11-29 15:30:01 +0100

    Updated panels_prepro

--- python_files/Preprocessed Dataframe.py
+++ python_files/Preprocessed Dataframe.py
@@ -1,12 +1,16 @@
-
-import pandas as pd
-import seaborn as sns
 import pandas as pd
 
-def get_dataframe_option1() :
-    df = pd.read_csv("data/global_power_plant_database.csv", low_memory=False)
-    df = df[df['primary_fuel'] == 'Solar'].reset_index(drop=True)
-    df.drop(columns=['gppd_idnr', 'capacity_mw','other_fuel1', 'other_fuel2', 'other_fuel3', 'commissioning_year', 'owner', 'source', 'url',
+df = pd.read_csv("../raw_data/global_power_plant_database.csv")
+
+
+# 1. Dataframe option 1
+# only the longitude and latitude for all the plants
+
+
+def get_dataframe_option1 () :
+   df = pd.read_csv("../raw_data/global_power_plant_database.csv")
+   df = df[df['primary_fuel']== 'Solar'].reset_index(drop=True)
+   df.drop(columns=['gppd_idnr', 'capacity_mw','other_fuel1', 'other_fuel2', 'other_fuel3', 'commissioning_year', 'owner', 'source', 'url',
        'geolocation_source', 'wepp_id', 'year_of_capacity_data',
        'generation_gwh_2013', 'generation_gwh_2014', 'generation_gwh_2015',
        'generation_gwh_2016', 'generation_gwh_2017', 'generation_gwh_2018',
@@ -16,8 +20,10 @@ def get_dataframe_option1() :
        'estimated_generation_gwh_2017', 'estimated_generation_note_2013',
        'estimated_generation_note_2014', 'estimated_generation_note_2015',
        'estimated_generation_note_2016', 'estimated_generation_note_2017'], inplace=True)
-    return df
+   return df
 
+# 2. Dataframe option 2
+# all the plants + estimated production of 2016
 
 
 def get_dataframe_option2() :
